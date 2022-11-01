69653b2eaa4de5ca65bd7192fc106bc31731d504
Никита Остапенко <nikitaostapenko0968@gmail.com>
2022-11-01 11:01:12 +0300

    7 commit

--- homework.py
+++ homework.py
@@ -109,7 +109,7 @@ class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP: float = 1.38
     CALORIES_MEAN_SPEED_SHIFT: float = 1.1
-    CALORIES_MEAN_SPEED_MULTIPLIER: int = 2
+    CALORIES_WEIGHT_MULTIPLIER: int = 2
 
     def __init__(
         self,
@@ -131,7 +131,7 @@ class Swimming(Training):
     def get_spent_calories(self) -> float:
         return (
             (self.get_mean_speed() + self.CALORIES_MEAN_SPEED_SHIFT)
-            * self.CALORIES_MEAN_SPEED_MULTIPLIER
+            * self.CALORIES_WEIGHT_MULTIPLIER
             * self.weight
         )
 
